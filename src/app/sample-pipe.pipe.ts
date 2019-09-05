f918676826de6127ddc4ca44bec650c11cdcf373
bhargavkomali <bhargavareddy.komali@gmail.com>
2019-09-05 23:58:39 +0530

    filter_pipe

--- src/app/sample-pipe.pipe.ts
+++ src/app/sample-pipe.pipe.ts
@@ -5,18 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SamplePipePipe implements PipeTransform {
 
-  transform(value: any[], searchText:any, prop?:any): any {
+  transform(value: any[], searchText:any): any {
     //  this will return values to regarding component function
     // return value.fullname.toUpperCase();
     // console.log('from pipe.ts',value.fullname)
-    if(!value){
+    if(!value || !searchText){
       // console.log('in pipe.ts arr',value)
-      return [];
-    }
-    if(!searchText || !prop){
-      // console.log('in pipe.ts arr',searchText)
       return value;
     }
+     return value.filter(value_name_ =>value_name_.fullname.toLowerCase().indexOf(searchText.toLowerCase()) !== -1);
+    
     // kud venkat gives good
 
   }
